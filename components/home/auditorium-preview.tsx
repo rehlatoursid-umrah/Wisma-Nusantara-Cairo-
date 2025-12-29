3a86516895ff6768da9575d939ae2b4f34604fc6
rehlatoursid-umrah <rehlatours.id@gmail.com>
2025-12-29 13:00:01 +0200

    Update auditorium-preview.tsx

--- components/home/auditorium-preview.tsx
+++ components/home/auditorium-preview.tsx
@@ -13,7 +13,7 @@ export function AuditoriumPreview() {
       <div
         className="absolute inset-0 opacity-[0.03]"
         style={{
-          backgroundImage: `url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3z' fill='%23DBC596' fillOpacity='1' fillRule='evenodd'/%3E%3C/svg%3E")`,
+          backgroundImage: `url("https://raw.githubusercontent.com/rehlatoursid-umrah/Wisma-Nusantara-Cairo-/refs/heads/main/public/aula%202.png")`,
         }}
       />
 
